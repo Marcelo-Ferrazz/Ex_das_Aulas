89cb0d1b4e106a044698ab3b15c730c29f6094b9
Marcelo-Ferrazz <dferrazz@hotmail.com>
2022-10-10 20:57:08 -0300

    add

--- ex_JS/main.js
+++ ex_JS/main.js
@@ -1,6 +1,8 @@
-var portugues = 7
-var matematica = 7
-var ingles = 5
-var fisica = 5
+var portugues = 7;
+var matematica = 7;
+var ingles = 5;
+var fisica = 5;
 
-console.log("A média do aluno com as 4 notas é " + (portugues + matematica + ingles + fisica ) / (4))
+var media = (portugues + matematica + ingles + fisica) / 4;
+
+console.log("A média do aluno com as 4 notas é : " + ( media ));
