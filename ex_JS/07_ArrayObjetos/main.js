fadfa25fb3815772cb7690daf9cda9c6cc548456
Marcelo-Ferrazz <dferrazz@hotmail.com>
2022-10-18 21:34:04 -0300

    add ex

---
+++ ex_JS/07_ArrayObjetos/main.js
@@ -0,0 +1,12 @@
+var nome = "Jéssica "
+var nota1 = 7
+var nota2 = 6
+var nota1 = 7
+var nota2 = 6
+var mediaNotas = (nota1 + nota2) / 4
+console.log (mediaNotas)
+if (mediaNotas >= 6)
+console.log ("Aprovado")
+else
+console.log ("Reprovado")
+console.log (nome)
