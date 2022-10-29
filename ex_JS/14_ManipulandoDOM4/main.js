4f1469bff8acb49a2a00dd607324d10c05d5ff1a
Marcelo-Ferrazz <dferrazz@hotmail.com>
2022-10-28 21:04:29 -0300

    add ex

---
+++ ex_JS/14_ManipulandoDOM4/main.js
@@ -0,0 +1,12 @@
+// Método querySelector
+const btn = document.querySelector('.btn');
+btn.style.background = 'red';
+
+// Busca pelos elementos do Formulário
+const myForm = document.querySelector('#my-form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const horario = document.querySelector('#horario');
+const msg = document.querySelector('.msg');
+const msg_email = document.querySelector('.msg_email');
+const userList = document.querySelector('#users');
